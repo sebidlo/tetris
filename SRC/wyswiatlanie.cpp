b19138176a701d7609e15019cd36bd35cd1bcb69
sebidlo <sebidlo@gmail.com>
2023-01-04 15:15:41 +0100

    dodanie funkcji wyświetlanian tablicy

---
+++ SRC/wyswiatlanie.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include "konfiguracja.hpp"
+
+using namespace std;
+
+void wyswietl_tablice(int n,int m, int tablica[rozmiar_x][rozmiar_y])
+{
+    for(int y=1;y<n-1;y++)
+    {
+        for(int x=1;x<m-1;x++)
+        {
+        cout << tablica[x][y]<< " " ;
+        }
+        cout<<endl;
+    }
+}
